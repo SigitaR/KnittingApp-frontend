54cac40a20b70b5e369266724c419b11de7c43e0
andrijaitis <andrijaitis92@gmail.com>
2019-05-08 14:07:21 +0200

    handling expired token

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Routes, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,23 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (localStorage.getItem('token') != null) {
+    const token = localStorage.getItem('token'); // getting token from local storage
+    if (token != null && this.TokenNotExpired(token) ) { // checks if token exists and if token is expired
       return true;
     } else {
       this.routes.navigate(['/login']);
       return false;
     }
-
   }
+
+
+  public TokenNotExpired(token) {  // checks if token is expired
+   const decodenToken = decode(token);
+   const expiration = decodenToken.exp;
+   const currentTime =  Date.now();
+   const isExpired = new Date(expiration) <= new Date(currentTime); // comparing two unix dates
+   console.log(isExpired);
+   return isExpired;
+  }
+
 }
