afaabddf4d89bc93db52c21c6ad7a0182cc0348a
andrijaitis <andrijaitis92@gmail.com>
2019-05-09 23:50:23 +0200

    added server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://ourLinuxServer:3000/api', // musu linux serveris
-  IMAGES: 'http://ourLinuxServer:3000/images' // musu linux serveris
+  API_URL: 'http://134.209.247.255:3000/api/', // musu linux serveris
+  IMAGES: 'http://134.209.247.255:3000/images/' // musu linux serveris
 
 };
