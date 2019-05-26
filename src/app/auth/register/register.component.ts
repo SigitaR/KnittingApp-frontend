53f7bcd3f5bf46344931518bf0b67a28a8629497
andrijaitis <andrijaitis92@gmail.com>
2019-05-26 21:48:52 +0200

    small fix captcha

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -12,31 +12,25 @@ import { AuthService } from 'src/services/auth.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  // @ViewChild('g-recaptcha-response') recaptcha: ElementRef;
-  // public registerForm;
-  // public captcha = this.elRef.nativeElement.querySelector('g-recaptcha-response');
 
   public name;
   public email;
   public password;
   public captcha;
+
   register(registerForm) {
-    this.captcha = document.querySelector('#g-recaptcha-response').value;
-    // console.log(this.recaptcha.nativeElement.value);
-    // console.log(document.querySelector('#g-recaptcha-response').value); // yra value nx
-    // registerForm.captcha = document.querySelector('#g-recaptcha-response').value;
     if (registerForm.valid) {
 
       registerForm.name = this.name;
       registerForm.email = this.email;
       registerForm.password = this.password;
-      registerForm.captcha = this.captcha;
-      // registerForm.captcha = "123445";
+      // registerForm.captcha = this.elRef.nativeElement.querySelector('#g-recaptcha-response').value;
+      // registerForm.captcha = document.querySelector('#g-recaptcha-response').value;
 
-      const user: User = registerForm.value;
+      const user: any = registerForm.value;
+      user.captcha = this.elRef.nativeElement.querySelector('#g-recaptcha-response').value;
       this.authService.registerUser(user).subscribe(() => this.router.navigate(['login']));
     } else {
-      // cia koki modal galesim parodyti, bet kolkas alert
       alert('bad credentials');
     }
   }
